225d36af01931f3a3beb5c6d3e2e434886fb60e2
anastasia <anastasia.aizman@gmail.com>
2022-01-22 15:39:02 -0500

    update - rules

--- backend/app/rules.py
+++ backend/app/rules.py
@@ -13,96 +13,165 @@ floor = {
     # main floor
     "0": ["Find yourself on the main floor of Hayden Library.", "Amble around the library on the "
                                                                 "first floor", "Navigate to the "
-                                                                               "Cafeteria",
+                                                                               "Courtyard Cafe.",
           "Admire the view from the expansive windows of the Hayden Library."],
     # downstairs floor
-    "1": ["Go to the stacks floor of Hayden Library.", "Descend to the bowels of Hayden library",
+    "1": ["Go to the stacks floor of Hayden Library.",
+          "Descend to the bowels of Hayden Library.",
           "Where are the stacks? We must find them."]
 }
 
-position = {
+stack = {
     # main floor
-    "0": ["Are you on the side of the Cafeteria? Stroll to the other side.", "Find the "
-                                                                             "...something"],
-    "1": ["Walk to the middle of the room.", "So many stacks! Find your favorite one."]
+    "0": ["Are you on the side of the Courtyard Cafe? Stroll to the other side.",
+          # "Scurry towards the Courtyard Cafe.",
+          "Walk to the middle of the room.",
+          "Go to the stacks on the side of the Courtyard Cafe",
+          "Go to the stacks behind the Cafeteria",
+          "function:robot"],
+    "1": ["Walk to the middle of the room.", "So many stacks! Find your favorite one.",
+          "Go to the closest stack.", "Get to the stack across the room.",
+          "function:robot"]
 }
 
+shelf = {
+    "0": ["function:robot"],
+    "1": ["function:robot"]
+}
+
+book = {
+    "0": ["function:robot"],
+    "1": ["function:robot"]
+}
+
+book_options = ["Find a big book.", "Pick up a small book.",
+                "Search for a book whose jacket is closest to your favorite color.",
+                "Find a book with the loveliest title.",
+                "Find a book with a title that reminds you of yesterday.",
+                "Find a book with a title that reminds you of your favorite person.",
+                "Find a book with a title that sounds heavy.", "Find an orange book.",
+                "Find a book with a title that sounds light.", "Find the book that finds you.",
+                "Find a blue book.", "Pick up a book with a jacket the color of the sky."]
+
+book["0"] += book_options
+book["1"] += book_options
+
+book_part = {
+    "0": ["function:robot"],
+    "1": ["function:robot"]
+}
+
+book_part_options = ["Flip three times.",
+                     "Flip four times.",
+                     "Flip five pages",
+                     "Flip three pages from the back.",
+                     "Go to page twelve.", ]
+
+book_part["0"] += book_part_options
+book_part["1"] += book_part_options
+end = {
+    "0": ["Find a sentence or a fragment of a sentence that you like to continue the poem."],
+    "1": ["Find a sentence or a fragment of a sentence that you like to continue the poem."]
+}
 choices = {
     "floor": floor,
-    "position": position
+    "stack": stack,
+    "shelf": shelf,
+    "book": book,
+    "book_part": book_part,
+    "end": end,
+}
+
+# robot options
+options = {
+    "0": {
+        "genres": ["Travel section"],
+        "stack": 5,
+        "shelf": 3,
+        "book": 10,
+        "book_part": ["beginning", "middle", "end"],
+
+    },
+    "1": {
+        "stack": 249,
+        "shelf": 6,
+        "book": 10,
+        "book_part": ["beginning", "middle", "end"],
+
+    }
 }
 
 
 class Rules:
     def __init__(self):
-        self.step = 0
-        self.floor = ["main", "downstairs"]
-        self.main_side = ["Go to the stacks on the side of the cafeteria", "Go to the stacks "
-                                                                           "behind the cafeteria"]
-        self.downstairs_stacks_num = 249
-        self.main_stacks_num = 5
-        self.main_shelf_num = 3
-        self.downstairs_shelf_num = 6
-        self.book_nums = 10
-        self.triple = ["beginning", "middle", "end"]
-        self.top_or_bottom = ["top", "bottom"]
         self.all = []
-        # choose floor
-        # if self.floor == 0:
-        #     self.rules_for_main_floor()
-        # elif self.floor == 1:
-        #     self.rules_for_downstairs()
         self.floor = roll_dice(sides=2)
-        self.all.append(self.choose("floor"))
-        self.all.append(self.choose("position"))
+        choice = self.choose("floor")
+        choice = self.expand(choice, "floor")
+        self.all.append(choice)
+        # choice = self.choose("stack")
+        # choice = self.expand(choice, "position")
+        # self.all.append(choice)
+        steps = roll_dice(3)
+        # if steps is 0, we just need to pick a book
 
-        # choose book num
-        book_num = random.randint(1, self.book_nums)
-        suffixed_num = f"{get_suffix(book_num)}"
-        self.all.append(f"Get the {suffixed_num} book you see from the shelf.")
-        self.step = 4
-        # choose part of book
-        book_part = random.choice(self.triple)
-        self.all.append(f"Flip to the {book_part} of the book")
-        self.step = 5
-        # phrase instruction
-        self.all.append(
-            "Find a sentence or a fragment of a sentence that you like to continue the poem.")
-        self.step = 6
+        end = self.choose("end")
+        end = self.expand(end, "end")
 
-    def rules_for_main_floor(self):
-        # choose stacks side if on main floor
-        side = random.choice(self.main_side)
-        self.all.append(side)
-        # choose stack num
-        stacks_num = random.randint(1, self.main_stacks_num)
-        self.all.append(f"Go to stack number {stacks_num}")
-        self.step = 2
-        # choose shelf num
-        shelf_num = random.randint(1, self.main_shelf_num)
-        direction = random.choice(self.top_or_bottom)
-        shelf_suffixed = f"{get_suffix(shelf_num)}"
-        self.all.append(f"Look at the {shelf_suffixed} shelf from the {direction}")
-        self.step = 3
+        if steps == 0:
+            choice = self.choose("book")
+            choice = self.expand(choice, "book")
+            self.all.append(choice)
+            self.all.append(end)
+            return
 
-    def rules_for_downstairs(self):
-        # choose stack num
-        stacks_num = random.randint(1, self.downstairs_stacks_num)
-        self.all.append(f"Go to stack number {stacks_num}")
-        self.step = 2
-        # choose shelf num
-        shelf_num = random.randint(1, self.downstairs_shelf_num)
-        direction = random.choice(self.top_or_bottom)
-        shelf_suffixed = f"{get_suffix(shelf_num)}"
-        self.all.append(f"Look at the {shelf_suffixed} shelf from the {direction}")
-        self.step = 3
+        # if steps is 1, stack + book
+        choice = self.choose("stack")
+        choice = self.expand(choice, "stack")
+        self.all.append(choice)
+        choice = self.choose("book")
+        choice = self.expand(choice, "book")
+        self.all.append(choice)
+        if steps == 1:
+            self.all.append(end)
+            return
+
+        # if steps is 2, stack + book + part of book
+        choice = self.choose("book_part")
+        choice = self.expand(choice, "book_part")
+        self.all.append(choice)
+        self.all.append(end)
+        return
 
     def choose_book(self):
         choice = roll_dice()
         if choice == 0:
             self.robot_rules()
 
-    def robot_rules(self):
+    def expand(self, choice, step):
+        """Run function if function chosen"""
+        if "function" not in choice:
+            return choice
+        func_name = choice.split("function:")[1]
+        # return func_name
+        if func_name == "robot":
+            return self.robot_rules(step)
+        return func_name
+
+    def robot_rules(self, step):
+        if step == "book_part":
+            choice = roll_dice(sides=len(options[str(self.floor)][step]))
+            return f"Flip to the {options[str(self.floor)][step][choice]} of the book."
+
+        num_of_options = options[str(self.floor)][step]
+        choice = roll_dice(num_of_options)
+        if step == "stack":
+            return f"Go to stack number {choice}."
+
+        suffixed = get_suffix(choice)
+        if step == "book":
+            return f"Pick up the {suffixed} book you see."
+
         pass
 
     def __str__(self):
