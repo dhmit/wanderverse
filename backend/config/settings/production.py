582a58c4a229e4522930c12bcb264b75f5601820
Ubuntu <ubuntu@ip-172-31-5-23.us-west-1.compute.internal>
2022-03-15 12:30:16 +0000

    deploy

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -11,8 +11,5 @@ DEBUG = False
 
 # SECRET_KEY = os.environ['DJANGO_SECRET_KEY']  # set in venv activate
 
-ADMINS = ['rahmed@mit.edu']  # Django will email Ryaan on internal server errors
-
-ALLOWED_HOSTS = []
 
 CORS_ORIGIN_WHITELIST = []
