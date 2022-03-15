76f18f17f20af50c76f380552a67244e7f944b30
anastasia <anastasia.aizman@gmail.com>
2022-03-15 15:09:55 -0400

    add - about

--- frontend/components/About.js
+++ frontend/components/About.js
@@ -1,12 +1,16 @@
 import React from "react";
 import W from "../images/icons/w.svg";
+import Logo from "../images/logo.svg";
 
 
 class About extends React.Component {
     render() {
-        return <div className={"text-center"}>
-            <a href={"/"}><W height={"80px"} className={"2 mb-4"}/></a>
-            <div>This is About</div>
+        return <div className={"about"}>
+            <a href={"/"}>
+                <Logo className={"2 mb-4"}/>
+            </a>
+            <div>The Wanderverse Project is an exquisite corpse poetry treasure hunt game. Each poem is called a “wanderverse.” The purpose of this project is to draw the MIT public in to explore spaces in the library they wouldn't otherwise get a chance to visit, and to promote stack browsing.
+</div>
         </div>;
     }
 }
