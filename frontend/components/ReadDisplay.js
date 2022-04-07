515b552ae748208b323f5d19fc7db717479de507
anastasia <anastasia.aizman@gmail.com>
2022-04-07 09:17:34 -0400

    bump - adding new verses works but it's all very jumpy. trying a different tactic

--- frontend/components/ReadDisplay.js
+++ frontend/components/ReadDisplay.js
@@ -1,12 +1,19 @@
-import React, {useRef} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import * as PropTypes from "prop-types";
 import Logo from "../images/logo.svg";
+import Squiggle from "../images/icons/squiggle.svg";
+import axios from "axios";
 
 
 const ReadDisplay = ({data}) => {
     // adding line dividers until the last line of the poem
-    const content = JSON.parse(data.verses);
     const messagesEndRef = useRef(null);
+    const containerRef = useRef(null);
+    const [wanderverses, setWanderverses] = useState([]);
+    const [wanderverseElements, setWanderverseElements] = useState([]);
+    const [newWanderverses, setNewWanderverses] = useState([]);
+
+    // setWanderverses(JSON.parse(data.verses));
 
     const getInfo = (line, idx) => {
         let infoText = "("
@@ -27,57 +34,90 @@ const ReadDisplay = ({data}) => {
         </span>
     }
 
-    const verses = content.map((line, idx) => {
-        let info = getInfo(line, idx);
-        return <>
-            <li className={"verse-container"} key={`verse-container-${idx}`}>
-                <span key={`verse-${idx}`} className={"verse"}>
+    const constructWanderverseList = () => {
+        let newWans;
+        if (wanderverses.length < 20) {
+            console.log("wanderverses length < 20==========", wanderverses);
+            newWans = [...wanderverses, ...newWanderverses];
+        } else {
+            console.log("wanderverses length >>>> 20==========");
+            newWans = [...wanderverses.slice(10, wanderverses.length - 10), ...newWanderverses];
+        }
+        console.log("constructWanderverseList newWans", newWans);
+        setWanderverses(newWans);
+        constructWanderverseElements();
+        return newWans
+    }
+
+    const constructWanderverseElements = () => {
+        let wL = wanderverses.map(line => {
+            let info = getInfo(line, line.id);
+            return <>
+                <li className={"verse-container"} key={`verse-container-${line.id}`}>
+                <span key={`verse-${line.id}`} className={"verse"}>
                         {line.text} {info} <span className={"text-citation"}>/</span>
                 </span>
-            </li>
-        </>;
-    });
+                </li>
+            </>;
+        });
+        console.log("!!!!constructWanderverseElements", wL);
+        setWanderverseElements(wL);
+    }
+
+    // const fix
+
+    const getWanderverses = () => {
+        axios.get("/random").then((res) => {
+            return JSON.parse(res.data.verses);
+        }).then((v) => {
+            return setNewWanderverses(v);
+        });
+    }
+
+    useEffect(() => {
+        console.log("data!!!", data);
+        console.log("setWanderverses called on load", wanderverses);
+        setInterval(getWanderverses, 6000);
+    }, []);
+
+    useEffect(() => {
+        newWanderverses && newWanderverses.length && constructWanderverseList();
+
+    }, [newWanderverses]);
+
+    useEffect(() => {
+
+    }, []);
 
     return (
         <div id="display" className={"pt-4 pl-4 pr-4 pb-2"}>
             <div className={"left-side"}>
-                <div
-                    className={"logo-container"}>
-                    <Logo className={"w ml-3 mb-5"} fill={"#0C00FF"}/>
+                <div className={"logo-container"}>
+                    <Logo className={"w ml-3 mb-5"} fill="#0C00FF"/>
                     <p>
                         <a href={"/"}>wanderverse.dhlab.mit.edu</a>
+                        <br/>
+                        <Squiggle width="190px" fill="#0C00FF"/>
                     </p>
                 </div>
             </div>
-            <div className={"right-side"}>
-                <div className="wanderverse-container text-left">
-                    <div className={"new-verse marquee"}>
+            <div className="right-side">
+                <div ref={containerRef} className="wanderverse-container text-left">
+                    <div className="new-verse marquee">
                         <ul className="list">
-                            <li className={"verse-container"}>
-                            <span className={"verse"}>
-Minima officiis praesentium cumque ipsam deleniti sit sed. Et neque cum quia dolorem doloremque. Enim debitis nam nihil. Error nisi aut ut dignissimos enim impedit voluptas occaecati. Quo et ex voluptatem.
-
-Velit qui nisi expedita. Quod velit quisquam accusantium fugiat qui velit aut at. Sunt aut nihil et quibusdam. Est quam excepturi dolorum totam facere perspiciatis dolorem. Rem in accusantium eos quasi hic. Soluta quia occaecati ut autem.
-
-Consequatur esse sit quaerat omnis et non eum. Dignissimos ut unde minima. Voluptates non porro atque dolor dolores quia sit.
-
-Et voluptas sequi eos repudiandae quos non. Ea atque doloremque unde enim. Aut deserunt sequi nihil est quia quo maiores illum. Facere tempore commodi dicta. Numquam ipsum ab magnam quo voluptas earum. Sit libero dolor error atque eaque laboriosam aut.
-
-Suscipit ipsa impedit nulla pariatur ducimus rerum. Cupiditate quia maxime voluptatem explicabo nobis. Tempora aliquam maiores amet minima rem commodi est occaecati. Et repellendus omnis veritatis.
- <span className={"text-citation"}>/</span>
-                </span>
+                            <li className="verse-container">
+                                <span className="verse">
+                                    <span className="text-citation">/</span>
+                                </span>
                             </li>
-                            {verses}
-                            <div
-                                ref={messagesEndRef}>
-                            </div>
+                            {wanderverseElements}
+                            <div ref={messagesEndRef}/>
                         </ul>
                     </div>
                 </div>
             </div>
         </div>
-    )
-        ;
+    );
 };
 
 ReadDisplay.propTypes = {
