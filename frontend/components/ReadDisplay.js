de820fbf994d3d842eafdfa1caee73c0d1198eb5
anastasia <anastasia.aizman@gmail.com>
2022-04-22 22:11:10 -0400

    update - bad display

--- frontend/components/ReadDisplay.js
+++ frontend/components/ReadDisplay.js
@@ -1,19 +1,35 @@
 import React, {useEffect, useRef, useState} from "react";
 import * as PropTypes from "prop-types";
 import Logo from "../images/logo.svg";
-import Squiggle from "../images/icons/squiggle.svg";
+import QR from "../images/QR.svg";
+import Symbol from "./Symbol";
 import axios from "axios";
 
 
 const ReadDisplay = ({data}) => {
     // adding line dividers until the last line of the poem
-    const messagesEndRef = useRef(null);
+    const endOfWanderverseRef = useRef(null);
     const containerRef = useRef(null);
+    const listRef = useRef(null);
+    const symbolsContainerRef = useRef(null);
     const [wanderverses, setWanderverses] = useState([]);
     const [wanderverseElements, setWanderverseElements] = useState([]);
-    const [newWanderverses, setNewWanderverses] = useState([]);
+    const [symbols, setSymbols] = useState([]);
+    const [dissolveClass, setDissolveClass] = useState("");
+    const [stopMarqueeClass, setStopMarqueeClass] = useState("");
+    const [listClass, setListClass] = useState("list");
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
+    const startingStyle = {
+        "-moz-transform": "translateY(0%)",
+        "-webkit-transform": "translateY(0%)",
+        "transform": "translateY(0%)"
+    }
 
-    // setWanderverses(JSON.parse(data.verses));
+
+    const randomInt = (max, min = 0) => {
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
 
     const getInfo = (line, idx) => {
         let infoText = "("
@@ -34,84 +50,122 @@ const ReadDisplay = ({data}) => {
         </span>
     }
 
-    const constructWanderverseList = () => {
-        let newWans;
-        if (wanderverses.length < 20) {
-            console.log("wanderverses length < 20==========", wanderverses);
-            newWans = [...wanderverses, ...newWanderverses];
-        } else {
-            console.log("wanderverses length >>>> 20==========");
-            newWans = [...wanderverses.slice(10, wanderverses.length - 10), ...newWanderverses];
-        }
-        console.log("constructWanderverseList newWans", newWans);
-        setWanderverses(newWans);
-        constructWanderverseElements();
-        return newWans
-    }
-
     const constructWanderverseElements = () => {
         let wL = wanderverses.map(line => {
             let info = getInfo(line, line.id);
             return <>
                 <li className={"verse-container"} key={`verse-container-${line.id}`}>
-                <span key={`verse-${line.id}`} className={"verse"}>
-                        {line.text} {info} <span className={"text-citation"}>/</span>
-                </span>
+                    <span key={`verse-${line.id}`} className={"verse"}>
+                            {line.text} {info}
+                    </span>
                 </li>
             </>;
         });
-        console.log("!!!!constructWanderverseElements", wL);
         setWanderverseElements(wL);
     }
 
-    // const fix
-
     const getWanderverses = () => {
-        axios.get("/random").then((res) => {
-            return JSON.parse(res.data.verses);
-        }).then((v) => {
-            return setNewWanderverses(v);
+        return axios.get("/random")
+            .then((res) => {
+                return JSON.parse(res.data.verses);
+            });
+    }
+
+    const scroll = () => {
+        if (!inView()) {
+            console.log("not in view", listRef.current.style["margin-top"])
+            listRef.current.style["margin-top"] = listRef.current.style["margin-top"] ?
+                (Number(listRef.current.style["margin-top"].split("px")[0]) - 2) + "px"
+                : "0px";
+        }
+    }
+    useEffect(() => {
+        setInterval(() => {
+            scroll();
+        }, 35);
+    }, []);
+    const coverUp = () => {
+        // let count = 0;
+        // let delay = 0;
+        // let symbolEls = [];
+        // let randomNumSymbols = randomInt(500, 1000);
+
+        // while (count < randomNumSymbols) {
+        //     count += 1;
+        //     delay = 2000 + count;
+        //     let height = 20;
+        //     if (count > 200 && count < 500) {
+        //         height = randomInt(count / 2);
+        //     } else if (count > 500) {
+        //         height = randomInt(count / 2.5);
+        //     }
+        //     symbolEls.push(<Symbol extraClass="coverup-symbol"
+        //                            key={`count-${count}`} top={randomInt(windowHeight) + "px"}
+        //                            left={randomInt(windowWidth) + "px"}
+        //                            height={"" + height}
+        //                            delayDisplay={delay}/>);
+        // }
+
+        // setSymbols(symbolEls);
+        console.log("delay is now:", delay);
+        setTimeout(() => {
+            console.log("setDissolveClass hide");
+            setDissolveClass("hide");
+            // console.log("setStopMarqueeClass", "stop");
+            // setStopMarqueeClass("stop");
+            getWanderverses();
         });
     }
 
+    const inView = () => {
+        let bounding = endOfWanderverseRef.current.getBoundingClientRect();
+        let containerBounding = containerRef.current.getBoundingClientRect();
+        return bounding.bottom <= containerBounding.bottom;
+    }
+    //
+    // useEffect(() => {
+    //     const interval = setInterval(() => {
+    //         checkAndCoverup();
+    //     }, 1000);
+    //
+    //     const checkAndCoverup = () => {
+    //         if (inView()) {
+    //             coverUp();
+    //             window.clearInterval(interval);
+    //         }
+    //     }
+    // }, [wanderverseElements]);
+
     useEffect(() => {
-        console.log("data!!!", data);
-        console.log("setWanderverses called on load", wanderverses);
-        setInterval(getWanderverses, 6000);
+        setWanderverses(JSON.parse(data.verses));
     }, []);
 
     useEffect(() => {
-        newWanderverses && newWanderverses.length && constructWanderverseList();
-
-    }, [newWanderverses]);
-
-    useEffect(() => {
-
-    }, []);
+        wanderverses && wanderverses.length && constructWanderverseElements();
+    }, [wanderverses]);
 
     return (
         <div id="display" className={"pt-4 pl-4 pr-4 pb-2"}>
             <div className={"left-side"}>
                 <div className={"logo-container"}>
                     <Logo className={"w ml-3 mb-5"} fill="#0C00FF"/>
-                    <p>
-                        <a href={"/"}>wanderverse.dhlab.mit.edu</a>
-                        <br/>
-                        <Squiggle width="190px" fill="#0C00FF"/>
-                    </p>
+                    <br/><br/><br/><br/>
+                    <QR fill="#0C00FF" height="100px"/>
                 </div>
             </div>
             <div className="right-side">
+                <div id="symbols-container" className={dissolveClass}
+                     ref={symbolsContainerRef}>
+                    {symbols}
+                </div>
                 <div ref={containerRef} className="wanderverse-container text-left">
-                    <div className="new-verse marquee">
-                        <ul className="list">
+                    <div className={`marquee ${dissolveClass} ${stopMarqueeClass}`}>
+                        <ul ref={listRef} className={"wanderverse"}>
                             <li className="verse-container">
-                                <span className="verse">
-                                    <span className="text-citation">/</span>
-                                </span>
+                                <span className="verse"/>
                             </li>
                             {wanderverseElements}
-                            <div ref={messagesEndRef}/>
+                            <div id="end" ref={endOfWanderverseRef}> HELLO END OF WANDERVERSE</div>
                         </ul>
                     </div>
                 </div>
@@ -125,3 +179,18 @@ ReadDisplay.propTypes = {
 };
 
 export default ReadDisplay;
+
+//
+// .then((verses) => {
+//             console.log("gotten verses", verses);
+//             setWanderverses(verses);
+//             setListClass("hide");
+//             setTimeout(() => {
+//                 setListClass("list");
+//                 setDissolveClass("");
+//                 setStopMarqueeClass("");
+//                 setSymbols([]);
+//                 // setTimeout(() => {
+//                 // }, 300)
+//             }, 1000);
+//         });
