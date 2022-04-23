e3eb198c983778a601fb1445275655ddb5e1238b
anastasia <anastasia.aizman@gmail.com>
2022-04-23 16:56:21 -0400

    fix - scrolling and refreshing woo

--- frontend/components/ReadDisplay.js
+++ frontend/components/ReadDisplay.js
@@ -17,15 +17,10 @@ const ReadDisplay = ({data}) => {
     const [symbols, setSymbols] = useState([]);
     const [dissolveClass, setDissolveClass] = useState("");
     const [stopMarqueeClass, setStopMarqueeClass] = useState("");
-    const [listClass, setListClass] = useState("list");
     const windowWidth = window.innerWidth;
     const windowHeight = window.innerHeight;
-    const startingStyle = {
-        "-moz-transform": "translateY(0%)",
-        "-webkit-transform": "translateY(0%)",
-        "transform": "translateY(0%)"
-    }
 
+    let scrollIntervalID;
 
     const randomInt = (max, min = 0) => {
         return Math.floor(Math.random() * (max - min)) + min;
@@ -46,8 +41,8 @@ const ReadDisplay = ({data}) => {
         infoText = infoText === "()" ? "" : infoText;
         return <span key={`info-${idx}`}
                      className={"text-citation"}>
-            {infoText}
-        </span>
+                {infoText}
+            </span>
     }
 
     const constructWanderverseElements = () => {
@@ -55,9 +50,9 @@ const ReadDisplay = ({data}) => {
             let info = getInfo(line, line.id);
             return <>
                 <li className={"verse-container"} key={`verse-container-${line.id}`}>
-                    <span key={`verse-${line.id}`} className={"verse"}>
-                            {line.text} {info}
-                    </span>
+                        <span key={`verse-${line.id}`} className={"verse"}>
+                                {line.text} {info}
+                        </span>
                 </li>
             </>;
         });
@@ -71,78 +66,93 @@ const ReadDisplay = ({data}) => {
             });
     }
 
+    const verseOutOfSight = () => {
+        return containerRef.current.getBoundingClientRect().bottom + "px";
+    }
+
     const scroll = () => {
-        if (!inView()) {
-            console.log("not in view", listRef.current.style["margin-top"])
-            listRef.current.style["margin-top"] = listRef.current.style["margin-top"] ?
-                (Number(listRef.current.style["margin-top"].split("px")[0]) - 2) + "px"
-                : "0px";
+        if (!hasLeftTheStage()) {
+            listRef.current.style["top"] =
+                listRef.current.style["top"]
+                    ? (Number(listRef.current.style["top"].split("px")[0]) - 1) + "px"
+                    : verseOutOfSight();
+
+        } else {
+            console.log("scrollIntervalID", scrollIntervalID);
+            window.clearInterval(scrollIntervalID);
+            listRef.current.style["display"] = "none";
+            listRef.current.style["top"] = verseOutOfSight();
+            coverUp().then((r) => {
+                getWanderverses().then((res) => {
+                    setSymbols([]);
+                    setTimeout(() => {
+                        setWanderverses(res);
+                        listRef.current.style["display"] = "block";
+                    }, 200);
+                });
+            });
         }
     }
-    useEffect(() => {
-        setInterval(() => {
-            scroll();
-        }, 35);
-    }, []);
+
     const coverUp = () => {
-        // let count = 0;
-        // let delay = 0;
-        // let symbolEls = [];
-        // let randomNumSymbols = randomInt(500, 1000);
+        let count = 0;
+        let delay = 0;
+        let symbolEls = [];
+        let randomNumSymbols = randomInt(300, 500);
+        while (count < randomNumSymbols) {
+            count += 1;
+            delay = 2000 + count;
+            let height = 20;
+            if (count > 200 && count < 500) {
+                height = randomInt(count / 2);
+            } else if (count > 500) {
+                height = randomInt(count / 2.5);
+            }
+            symbolEls.push(<Symbol extraClass="coverup-symbol"
+                                   key={`count-${count}`} top={randomInt(windowHeight) + "px"}
+                                   left={randomInt(windowWidth) + "px"}
+                                   height={"" + height}
+                                   delayDisplay={delay}/>);
+        }
 
-        // while (count < randomNumSymbols) {
-        //     count += 1;
-        //     delay = 2000 + count;
-        //     let height = 20;
-        //     if (count > 200 && count < 500) {
-        //         height = randomInt(count / 2);
-        //     } else if (count > 500) {
-        //         height = randomInt(count / 2.5);
-        //     }
-        //     symbolEls.push(<Symbol extraClass="coverup-symbol"
-        //                            key={`count-${count}`} top={randomInt(windowHeight) + "px"}
-        //                            left={randomInt(windowWidth) + "px"}
-        //                            height={"" + height}
-        //                            delayDisplay={delay}/>);
-        // }
-
-        // setSymbols(symbolEls);
-        console.log("delay is now:", delay);
-        setTimeout(() => {
-            console.log("setDissolveClass hide");
-            setDissolveClass("hide");
-            // console.log("setStopMarqueeClass", "stop");
-            // setStopMarqueeClass("stop");
-            getWanderverses();
-        });
+        setSymbols(symbolEls);
+        // console.log("delay is now:", delay);
+        // setTimeout(() => {
+        // console.log("setDissolveClass hide");
+        // setDissolveClass("hide");
+        // console.log("setStopMarqueeClass", "stop");
+        // setStopMarqueeClass("stop");
+        // getWanderverses();
+        // });
+        return new Promise((resolve) => {
+            setTimeout(resolve, delay + 2000);
+        })
     }
 
-    const inView = () => {
+    const hasLeftTheStage = () => {
         let bounding = endOfWanderverseRef.current.getBoundingClientRect();
         let containerBounding = containerRef.current.getBoundingClientRect();
-        return bounding.bottom <= containerBounding.bottom;
+        return containerBounding.top >= bounding.bottom;
     }
-    //
-    // useEffect(() => {
-    //     const interval = setInterval(() => {
-    //         checkAndCoverup();
-    //     }, 1000);
-    //
-    //     const checkAndCoverup = () => {
-    //         if (inView()) {
-    //             coverUp();
-    //             window.clearInterval(interval);
-    //         }
-    //     }
-    // }, [wanderverseElements]);
 
     useEffect(() => {
+        console.log("first useEffect")
         setWanderverses(JSON.parse(data.verses));
     }, []);
 
     useEffect(() => {
-        wanderverses && wanderverses.length && constructWanderverseElements();
+        wanderverses.length && constructWanderverseElements();
+        scrollIntervalID = setInterval(() => {
+            scroll();
+        }, 35);
     }, [wanderverses]);
+
+    // useEffect(() => {
+    //     scrollIntervalID = setInterval(() => {
+    //         scroll();
+    //     }, 35);
+    //
+    // }, [wanderverseElements]);
 
     return (
         <div id="display" className={"pt-4 pl-4 pr-4 pb-2"}>
@@ -154,18 +164,18 @@ const ReadDisplay = ({data}) => {
                 </div>
             </div>
             <div className="right-side">
-                <div id="symbols-container" className={dissolveClass}
-                     ref={symbolsContainerRef}>
-                    {symbols}
-                </div>
                 <div ref={containerRef} className="wanderverse-container text-left">
+                    <div id="symbols-container" className={dissolveClass}
+                         ref={symbolsContainerRef}>
+                        {symbols}
+                    </div>
                     <div className={`marquee ${dissolveClass} ${stopMarqueeClass}`}>
                         <ul ref={listRef} className={"wanderverse"}>
                             <li className="verse-container">
                                 <span className="verse"/>
                             </li>
                             {wanderverseElements}
-                            <div id="end" ref={endOfWanderverseRef}> HELLO END OF WANDERVERSE</div>
+                            <div id="end" ref={endOfWanderverseRef}/>
                         </ul>
                     </div>
                 </div>
