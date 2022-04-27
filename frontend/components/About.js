d315afdb7d79c4630a4f668035ba1f8d6e0d3f74
anastasia <anastasia.aizman@gmail.com>
2022-04-27 09:43:45 -0400

    update - about

--- frontend/components/About.js
+++ frontend/components/About.js
@@ -10,7 +10,8 @@ class About extends React.Component {
                 <Logo className="mb-4" height="10em" fill="white"/>
             </a>
             <div className="text-left">
-                <p>The Wanderverse Project is a collaborative poetry project in MIT's Hayden Library. Each
+                <p>The Wanderverse Project is a collaborative poetry project in MIT's Hayden
+                    Library. Each
                     co-created poem is called a “wanderverse.” The purpose of this project is to
                     draw the MIT public in to explore spaces in Hayden Library they wouldn't
                     otherwise get a chance to visit, and to promote stack browsing.
@@ -18,12 +19,12 @@ class About extends React.Component {
                 <p>
                     The Wanderverse Project is made with generous support from the <a
                     href={"https://arts.mit.edu/camit/"}>Council for the Arts at MIT</a>. It was
-                    created by Clare Stanton (<a href={"https://lil.law.harvard.edu"}>LIL</a>), Andy
-                    Silva (<a href={"https://lil.law.harvard.edu"}>LIL</a>), and Anastasia Aizman
+                    created by Anastasia Aizman
                     (<a href={"https://digitalhumanities.mit.edu/"}>DH Lab</a>
-                    ). It was built at MIT's <a href={"https://digitalhumanities.mit.edu/"}>
-                    Digital Humanities Lab</a> in collaboration with Ece Turnator and Mark Szarko of
-                    MIT Libraries.
+                    ), Clare Stanton (<a href={"https://lil.law.harvard.edu"}>LIL</a>), and Andy
+                    Silva (<a href={"https://lil.law.harvard.edu"}>LIL</a>). It was built at MIT's
+                    <a href={"https://digitalhumanities.mit.edu/"}> Digital Humanities Lab</a> in
+                    collaboration with Ece Turnator and Mark Szarko of MIT Libraries.
                 </p>
                 <p>
                     Say hi: <a href="mailto:wanderverse@mit.edu">wanderverse@mit.edu</a>.
