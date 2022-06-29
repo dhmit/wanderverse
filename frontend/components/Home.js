5e0eff386a5e9ec7e38ed536df7ddb6def48dc65
anastasia <anastasia.aizman@gmail.com>
2022-06-29 14:39:07 -0400

    update - moving to multiple locations, updating styles

--- frontend/components/Home.js
+++ frontend/components/Home.js
@@ -9,8 +9,11 @@ const Home = () => {
             <a href={"/"}>
                 <Logo height={"10em"} className={"mb-2 mt-4"} fill={"#3D2DFD"}/>
             </a>
-            <h3 className={"subtitle mb-5"}>A collaborative poetry project <br/>in MIT's Hayden Library.</h3>
-            <FancyButton title={"Play"}
+            <h3 className={"subtitle mb-5"}>A collaborative poetry project  </h3>
+            <FancyButton title={"Play at MIT"}
+                         href={"instructions/"}
+                         extraClass={"btn-primary"}/>
+            <FancyButton title={"Play everywhere else"}
                          href={"instructions/"}
                          extraClass={"btn-primary"}/>
             <FancyButton title={"Read"}
